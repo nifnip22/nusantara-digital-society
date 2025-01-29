7b1960a2fbf6082a0758e975b72c1ff3be408fa6
nifnip22 <proxigaming742@gmail.com>
2025-01-29 22:01:11 +0800

    Fetch upcoming event MDX to homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,8 +8,30 @@ import Link from 'next/link';
 import { Calendar, FileClock, MapPin } from 'lucide-react';
 import Image from 'next/image';
 import ArticleCarousel from '@/components/layouts/ArticleCarousel';
+import { promises as fs } from 'fs';
+import path from 'path';
+import { compileMDX } from 'next-mdx-remote/rsc';
 
-export default function Home() {
+export default async function Home() {
+	const upcomingEventFilePath = path.join(process.cwd(), 'src/mdx/events/upcoming-event', 'page.mdx');
+	const upcomingEventContent = await fs.readFile(upcomingEventFilePath, 'utf-8');
+
+	const { frontmatter } = await compileMDX<{
+		eventImageSrc: string;
+		eventTitle: string;
+		eventLocation: string;
+		eventDate: string;
+		eventTime: string;
+		registrationDeadlineDate: string;
+		registrationDeadlineTime: string;
+		registrationLink: string;
+	}>({
+		source: upcomingEventContent,
+		options: {
+			parseFrontmatter: true,
+		},
+	});
+
 	return (
 		<>
 			<div className={'bg-[#f9f2e9] relative'}>
@@ -93,9 +115,7 @@ export default function Home() {
 								<p className={'uppercase text-md md:text-lg font-semibold tracking-wide'}>Terbaru</p>
 								<hr className={'w-6 md:w-12 h-1 bg-white mb-4'} />
 								<h2 className={'mt-2 mb-4 text-xl md:text-2xl font-bold line-clamp-2 tracking-wide leading-normal'}>Judul Artikel</h2>
-								<p className={'text-sm md:text-md font-medium w-full line-clamp-3 tracking-wide leading-relaxed'}>
-									Deskripsi/Slug Artikel 
-								</p>
+								<p className={'text-sm md:text-md font-medium w-full line-clamp-3 tracking-wide leading-relaxed'}>Deskripsi/Slug Artikel</p>
 								<p className={'opacity-0 group-hover:opacity-100 duration-300 mt-0 group-hover:mt-4 inline-block text-sm md:text-md font-semibold text-white-500 hover:text-white-400 after:content-["_→"]'}>
 									Baca Selengkapnya
 								</p>
@@ -119,7 +139,7 @@ export default function Home() {
 					</div>
 					<div className={'flex flex-col 2xl:flex-row gap-y-16 2xl:gap-x-8 justify-center items-center'}>
 						<div className={'bg-gray-300 aspect-square w-full 2xl:max-w-4xl rounded-3xl overflow-hidden'}>
-							<Image src={'/img/image_placeholder.png'} alt={'Judul Event'} width={1500} height={1500} className={'w-full h-full object-cover'} />
+							<Image src={frontmatter.eventImageSrc} alt={frontmatter.eventTitle} width={1500} height={1500} className={'w-full h-full object-cover'} />
 						</div>
 						<div className={'flex flex-col gap-8 w-full 2xl:max-w-4xl'}>
 							<div className={'flex flex-col gap-4'}>
@@ -127,7 +147,7 @@ export default function Home() {
 									<p className={'uppercase text-sm md:text-lg text-[#D7AB6C] font-semibold'}>Upcoming</p>
 									<hr className={'w-12 h-1 bg-[#D7AB6C]'} />
 								</div>
-								<h1 className={'text-2xl md:text-3xl font-bold line-clamp-2 leading-normal tracking-wide'}>Judul Event</h1>
+								<h1 className={'text-2xl md:text-3xl font-bold line-clamp-2 leading-normal tracking-wide'}>{frontmatter.eventTitle}</h1>
 							</div>
 							<div className={'flex flex-col gap-4'}>
 								<div className={'group flex items-start gap-4 p-4 bg-gray-50 rounded-xl hover:bg-gray-100 transition-colors'}>
@@ -136,7 +156,7 @@ export default function Home() {
 									</div>
 									<div>
 										<p className={'text-xs md:text-sm font-semibold text-gray-500 group-hover:text-[#D7AB6C] transition-colors'}>Lokasi</p>
-										<p className={'text-base md:text-lg font-medium'}>Lokasi</p>
+										<p className={'text-base md:text-lg font-medium'}>{frontmatter.eventLocation}</p>
 									</div>
 								</div>
 								<div className={'group flex items-start gap-4 p-4 bg-gray-50 rounded-xl hover:bg-gray-100 transition-colors'}>
@@ -145,7 +165,9 @@ export default function Home() {
 									</div>
 									<div>
 										<p className={'text-xs md:text-sm font-semibold text-gray-500 group-hover:text-[#D7AB6C] transition-colors'}>Tanggal & Waktu</p>
-										<p className={'text-base md:text-lg font-medium'}>Tanggal | Waktu</p>
+										<p className={'text-base md:text-lg font-medium'}>
+											{frontmatter.eventDate} | {frontmatter.eventTime}
+										</p>
 									</div>
 								</div>
 								<div className={'group flex items-start gap-4 p-4 bg-gray-50 rounded-xl hover:bg-gray-100 transition-colors'}>
@@ -154,7 +176,9 @@ export default function Home() {
 									</div>
 									<div>
 										<p className={'text-xs md:text-sm font-semibold text-gray-500 group-hover:text-[#D7AB6C] transition-colors'}>Batas Waktu Pendaftaran</p>
-										<p className={'text-base md:text-lg font-medium'}>Tanggal | Waktu</p>
+										<p className={'text-base md:text-lg font-medium'}>
+											{frontmatter.registrationDeadlineDate} | {frontmatter.registrationDeadlineTime}
+										</p>
 									</div>
 								</div>
 							</div>
@@ -162,7 +186,9 @@ export default function Home() {
 								<Link href={'/upcoming-event'}>
 									<Button label={'Lebih Detail'} type={'button'} variant={'outline'} className={'w-full rounded-full after:content-["_→"]'} />
 								</Link>
-								<Button label='Registrasi Sekarang' type={'button'} variant={'dark'} className={'rounded-full after:content-["_→"]'} />
+								<Link href={frontmatter.registrationLink}>
+									<Button label='Registrasi Sekarang' type={'button'} variant={'dark'} className={'w-full rounded-full after:content-["_→"]'} />
+								</Link>
 							</div>
 						</div>
 					</div>
